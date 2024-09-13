7f12949429e76e90b1a563e5b68323dba2ee12a9
roshanwhive <roshanwhive@gmail.com>
2024-09-13 17:34:29 +0545

    first commit

---
+++ src/components/progress-bar/styles.js
@@ -0,0 +1,40 @@
+import GlobalStyles from '@mui/material/GlobalStyles';
+import { alpha, useTheme } from '@mui/material/styles';
+
+// ----------------------------------------------------------------------
+
+export default function StyledProgressBar() {
+  const theme = useTheme();
+
+  const inputGlobalStyles = (
+    <GlobalStyles
+      styles={{
+        '#nprogress': {
+          pointerEvents: 'none',
+          '.bar': {
+            top: 0,
+            left: 0,
+            height: 6.5,
+            zIndex: 9999,
+            width: '100%',
+            position: 'fixed',
+            backgroundColor: alpha(theme.palette.primary.main, 0.8),
+            boxShadow: `0 0 2px ${theme.palette.primary.main}`,
+          },
+          '.peg': {
+            right: 0,
+            opacity: 1,
+            width: 100,
+            height: '100%',
+            display: 'block',
+            position: 'absolute',
+            transform: 'rotate(3deg) translate(0px, -4px)',
+            boxShadow: `0 0 10px ${theme.palette.primary.main}, 0 0 5px ${theme.palette.primary.main}`,
+          },
+        },
+      }}
+    />
+  );
+
+  return inputGlobalStyles;
+}
